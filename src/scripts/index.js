c31e972afdaa12dfd20e7f41d054eaa0255039e2
dci-student <hugon1992@gmail.com>
2023-11-07 14:23:21 +0100

    new navbar added to all html-styled im style.css-js edited to responsive hided navbar

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+const swMenu = document.querySelector(".sw-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+swMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("hide");
+});
