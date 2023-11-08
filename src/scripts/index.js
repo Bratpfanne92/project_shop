77c76c31b2eba21e46ffc950b3f056495ca458a3
dci-student <hugon1992@gmail.com>
2023-11-08 15:16:20 +0100

    index.js edited-cartMenü open close onclick

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,3 @@
-// window.addEventListener("load", () => {
-
 //navbar toggle
 const swMenu = document.querySelector(".sw-menu");
 const navMenu = document.querySelector(".nav-menu");
@@ -7,3 +5,16 @@ const navMenu = document.querySelector(".nav-menu");
 swMenu.addEventListener("click", () => {
   navMenu.classList.toggle("hide");
 });
+
+//cart toggle
+let cartMenü = document.querySelector(".cart-menü");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector(".cart-x");
+// cartMenü open
+cartMenü.onclick = () => {
+  cart.classList.add("active");
+};
+// cartMenü close X
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
