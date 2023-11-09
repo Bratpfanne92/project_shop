aaa25f872f67834b8e6c34ad99b08eeab8232704
dci-student <hugon1992@gmail.com>
2023-11-09 11:09:56 +0100

    index.js edit- function for cart elemnte created-not successfully edited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,3 +18,47 @@ cartMenü.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove("active");
 };
+
+//cart working js-if document loaded dann ready() function sofort ausführen, sonst warten bis document geladen ist. Das ist wichtig, weil sonst die Funktionen nicht ausgeführt werden können, da die Elemente noch nicht geladen sind oder die Elemente noch nicht hinzugefügt wurden.zB.: Cart Elemente
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+//ready function
+
+function ready() {
+  //remove button-.cart remove-trash icon
+  let removeCartItemButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartItemButtons);
+  // for loop für trash button bei JEDE cart item
+  for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i];
+    button.addEventListener("click", removeCartItem); //onclick removeCartItem function
+  }
+}
+
+//removeCartItem function
+function removeCartItem(e) {
+  let buttonClicked = e.target; //.target ist für das element, das geklickt wurde,wo der event ausgeführt wurde
+  buttonClicked.parentElement.remove(); //parent von button löschen-hier .cart-box
+  updateCartTotal(); //update cart total function,wenn gelöscht der element
+}
+//update cart total function
+function updateCartTotal() {
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let cartProductPrice =
+      cartBox.getElementsByClassName("cart-product-price")[0];
+    let cartProductQuantity =
+      cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(cartProductPrice.innerText.replace("€", ""));
+    let quantity = cartProductQuantity.value;
+    total = total + quantity * price;
+
+    document.getElementsByClassName("total-price")[0].innerText = total + "€"; //oder total + €
+  }
+}
